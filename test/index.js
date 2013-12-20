d29162c0adb238d4d3f9d9087c46ab30d6fa0f29
Joaquim Serafim <joaquim.serafim@gmail.com>
2013-12-20 02:51:40 +0000

    start to use tape to do the testing stuff, change some things in Publish

--- test/index.js
+++ test/index.js
@@ -1,21 +1,38 @@
-
-var tape = require('tape');
-
-
+var test = require('tape');
+var Guid = require('guid');
+var _ = require('underscore');
 
 var Publisher = require('../').Publisher;
-
 
 var pub = new Publisher();
 
 
-pub.start(function (err) {
-  console.log(err, pub.info());
+test('client subscribing a channel don\'t exists', function (t) {
+  t.plan(1);
+
+  pub.start(function (err) {
+    t.equal(err !== null, true, err);
+  });
 });
 
 
-pub.publish('test', 'a new message 0');
-pub.publish('test', 'a new message 1');
-pub.publish('test', 'a new message 2');
-pub.publish('test', 'a new message 3');
-pub.publish('test', 'a new message 4');
+
+/*
+
+ // when server start to listener launch this method
+    // check you don't need this method to start publish messages
+    // it's used only if you want to confirm server is online
+    // create messages to publish
+    _.map(['test0'], function (channel) {
+      var n = _.random(100, 200);
+
+      t.plan(n + 1);
+      for (var i = 0; i < n; i++) {
+        var message = Guid.raw();
+        var res = pub.publish(channel, message);
+        t.equal(res, true, 'publish in channel ' + channel + ' a new message ' + message);
+      }
+    });
+
+
+*/
